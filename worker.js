0ccc3372570c3ac6a34e2d23934a23b49cd9fc9e
smt-ivr <s6754091@gmail.com>
2026-06-07 17:50:49 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -8,21 +8,18 @@ export default {
     const url = new URL(request.url);
     const path = url.pathname;
 
-    // ניתוב דף הבית (HTML)
     if (path === '/' || path === '/index.html') {
       return new Response(htmlContent, {
         headers: { 'Content-Type': 'text/html; charset=utf-8' },
       });
     }
 
-    // ניתוב קובץ העיצוב (CSS)
     if (path === '/style.css') {
       return new Response(cssContent, {
         headers: { 'Content-Type': 'text/css; charset=utf-8' },
       });
     }
 
-    // ניתוב קבצי הסקריפט (JS)
     if (path === '/mfa.js') {
       return new Response(mfaJsContent, {
         headers: { 'Content-Type': 'application/javascript; charset=utf-8' },
@@ -35,7 +32,6 @@ export default {
       });
     }
 
-    // אם הנתיב לא נמצא
     return new Response('404 Not Found', { status: 404 });
   },
 };
